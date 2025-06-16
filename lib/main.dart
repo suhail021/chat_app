df31b98572bdadcd7225f5c16fef4bffd61b5659
sohilalngm10@gmail.com <sohilalngm10@gmail.com>
2025-06-16 14:22:22 -0700

    create bloc auth and make loginpage run with bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google/views/blocs/auth_bloc/auth_bloc.dart';
 import 'package:google/views/cubits/auth_cubit/auth_cubit.dart';
 import 'package:google/views/cubits/chat_cubit/chat_cubit.dart';
 import 'package:google/firebase_options.dart';
@@ -22,6 +23,7 @@ class Chatapp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => AuthCubit()),
+        BlocProvider(create: (context) => AuthBloc()),
         BlocProvider(create: (context) => ChatCubit()),
       ],
       child: MaterialApp(
--- lib/views/loginpage.dart
+++ lib/views/loginpage.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google/constants.dart';
-import 'package:google/views/cubits/auth_cubit/auth_cubit.dart';
+import 'package:google/views/blocs/auth_bloc/auth_bloc.dart';
 import 'package:google/views/cubits/chat_cubit/chat_cubit.dart';
 import 'package:google/helper/showsnakbar.dart';
 import 'package:google/views/chat_page.dart';
@@ -21,7 +20,7 @@ class loginpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AuthCubit, AuthState>(
+    return BlocListener<AuthBloc, AuthState>(
       listener: (context, state) {
         if (state is loginpage) {
           Islooding = true;
@@ -91,9 +90,9 @@ class loginpage extends StatelessWidget {
                   color: Colors.white,
                   onTap: () async {
                     if (formkey.currentState!.validate()) {
-                      BlocProvider.of<AuthCubit>(
+                      BlocProvider.of<AuthBloc>(
                         context,
-                      ).loginuser(email: email!, password: password!);
+                      ).add(LoginEvent(email: email!, password: password!));
                     } else {}
                   },
                 ),
